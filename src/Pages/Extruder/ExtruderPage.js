5c94220efcfcbb8a6d837f2fc7f18d82648e51b3
FoxxMD <matt.duncan13@gmail.com>
2019-03-11 14:32:04 -0400

    Add querystring parsing to enable shareable links

--- src/Pages/Extruder/ExtruderPage.js
+++ src/Pages/Extruder/ExtruderPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ThreeRender from './ThreeRenderer';
 import { compose } from 'recompose';
 import isUrl from 'is-url';
+import qs from 'qs';
 import { CircularProgress, Paper, TextField, Button, Switch, withStyles } from '@material-ui/core';
 import pageHOC from '../PageHOC';
 import { connect } from 'react-redux';
@@ -46,12 +47,33 @@ const styles = theme => ({
 class ThreeContainer extends Component {
   constructor( props ){
 	super( props );
-	this.state    = {
+	const searchParams = qs.parse( window.location.search, { ignoreQueryPrefix: true } );
+	const {
+			url = 'https://i.imgur.com/3aDc8Iy.jpg',
+			h   = 0.05,
+			s   = 0,
+			v   = 0
+		  }            = searchParams || {};
+	
+	this.state = {
+	  initialLoad: true,
 	  loading: true,
-	  url: 'https://i.imgur.com/3aDc8Iy.jpg',
+	  urlInput: '',
+	  url,
 	  animate: true,
-	  messageOpen: false
+	  messageOpen: false,
+	  extrusion: {
+		h: typeof h === 'string' ? parseFloat( h ) : h,
+		s: typeof s === 'string' ? parseFloat( s ) : s,
+		v: typeof v === 'string' ? parseFloat( v ) : v,
+	  }
 	};
+	
+	if(!isUrl( url )) {
+	  this.state.messageOpen = false;
+	  this.state.message     = 'URL is not valid';
+	}
+	
 	this.renderer = null;
   }
   
@@ -62,22 +84,17 @@ class ThreeContainer extends Component {
 	  onReady: this.onReady,
 	  onProgress: this.setProgress,
 	  onError: this.setError,
-	  initialExtrusion: {
-		h: 0.05,
-		s: 0,
-		v: 0,
-	  }
-	} );
-	this.setState( {
-	  url: null
+	  initialExtrusion: this.state.extrusion,
 	} );
   }
   
   onReady = () => {
-	this.setState( {
+	console.log( 'ready' );
+	this.setState( prevState => ({
 	  loading: false,
-	  url: null,
-	} );
+	  url: prevState.initialLoad ? prevState.url : prevState.urlInput,
+	  urlInput: '',
+	}), this.setUrlHash );
   };
   
   handleToastClose = () => {
@@ -89,24 +106,48 @@ class ThreeContainer extends Component {
   
   setExtrusion = ( type, val ) => {
 	console.log( `Type: ${type}, Value: ${val}` );
+	this.setState( prevState => ({
+	  extrusion: {
+		...prevState.extrusion,
+		[ type ]: val
+	  }
+	}), this.setUrlHash );
 	this.renderer.setExtrusionTween( { [ type ]: val } );
   };
   
   setUrl = () => {
-	if(!isUrl( this.state.url )) {
+	if(!isUrl( this.state.urlInput )) {
 	  this.setState( {
 		message: 'URL is not valid',
 		messageOpen: true,
 	  } );
 	}
 	else {
-	  const url = this.state.url;
-	  this.renderer.setImage( url );
+	  this.renderer.setImage( this.state.urlInput );
+	}
+  };
+  
+  setUrlHash = () => {
+	const { url = '', extrusion: { s, h, v } = {}, initialLoad } = this.state;
+	if(initialLoad) {
+	  this.setState( {
+		initialLoad: false
+	  } );
+	}
+	else {
+	  const searchParams = new URLSearchParams();
+	  searchParams.set( 'url', url );
+	  searchParams.set( 'h', h.toLocaleString( undefined, { maximumFractionDigits: 2 } ) );
+	  searchParams.set( 's', s.toLocaleString( undefined, { maximumFractionDigits: 2 } ) );
+	  searchParams.set( 'v', v.toLocaleString( undefined, { maximumFractionDigits: 2 } ) );
+	  const newQs = searchParams.toString();
+	  window.history.replaceState( { newHash: newQs }, 'Extruder', `${window.location.origin}${window.location.pathname}?${newQs}` );
+	  
 	}
   };
   
   setProgress = () => {
-    console.log('progress');
+	console.log( 'progress' );
 	this.setState( {
 	  loading: true
 	} );
@@ -128,7 +169,8 @@ class ThreeContainer extends Component {
   };
   
   render(){
-	const { classes = {} } = this.props;
+	const { classes = {} }                = this.props;
+	const { extrusion: { h, s, v } = {}, urlInput } = this.state;
 	return (
 		<div style={{
 		  overflow: 'hidden',
@@ -147,7 +189,8 @@ class ThreeContainer extends Component {
 			<h4 style={{ margin: '10px 0 10px 0' }}>Settings</h4>
 			<TextField
 				classes={{ root: classes.textField }}
-				onChange={e => this.setState( { url: e.target.value } )}
+				value={urlInput}
+				onChange={e => this.setState( { urlInput: e.target.value } )}
 				id="filled-search"
 				label="URL"
 				type="search"
@@ -159,11 +202,11 @@ class ThreeContainer extends Component {
 			<div>
 			  Animate <Switch checked={this.state.animate} onChange={this.setAnimating}/>
 			</div>
-			<StatefulSlider max={3} initialValue={0.05} title="Hue" step={0.05}
+			<StatefulSlider max={3} initialValue={h} title="Hue" step={0.05}
 							onChange={( val ) => this.setExtrusion( EX_H, val )}/>
-			<StatefulSlider max={3} initialValue={0} title="Saturation" step={0.05}
+			<StatefulSlider max={3} initialValue={s} title="Saturation" step={0.05}
 							onChange={( val ) => this.setExtrusion( EX_S, val )}/>
-			<StatefulSlider max={3} initialValue={0} title="Brightness" step={0.05}
+			<StatefulSlider max={3} initialValue={v} title="Brightness" step={0.05}
 							onChange={( val ) => this.setExtrusion( EX_V, val )}/>
 		  </Paper>
 		  <div style={{ height: '100%', position: 'relative' }} ref={element => this.threeRootElement = element}/>
